86dac3381247717872d9a17b3f68f0363622fd9b
Stephanie Torisu <stephanie.torisu08@gmail.com>
2020-11-25 21:03:54 -0300

    add SplashActivity and drawables

---
+++ app/src/main/java/com/example/desafio3dh/SplashActivity.kt
@@ -0,0 +1,25 @@
+@file:Suppress("DEPRECATION")
+package com.example.desafio3dh
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, 2000)
+
+    }
+}
